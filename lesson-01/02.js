6863bcd104e0e313b559872662847bacc656f5f4
shoegaev <kshoegaev@gmail.com>
2024-09-10 14:14:47 +0300

    fix: lesson 01/02 fix

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,2 +1,9 @@
-let number1 = Math.floor(Math.random() * 100)
-console.log(`${number1}:Это ${number1 % 2 === 0 ? "четное" : "нечетное"} число`)
+let number1 = Math.floor(Math.random() * 100);
+// console.log(
+//   `${number1}:Это ${number1 % 2 === 0 ? "четное" : "нечетное"} число`,
+// );
+if (number1 % 2 === 0) {
+  console.log(number1, ":Это четное число");
+} else {
+  console.log(number1, ":Это нечетное число");
+}
